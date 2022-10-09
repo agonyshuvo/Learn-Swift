187e6a2380e0021fe02ce667b8183667db6588d6
Shuvo Khan <agonysuvo@gmail.com>
2022-10-09 21:21:09 +0600

    9 oct

--- lesson 3/user input.playground/Contents.swift
+++ lesson 3/user input.playground/Contents.swift
@@ -2,13 +2,20 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-let x = 11
+print("Enter a inter :")
 
-if (x % 2==0 ) {
-    
-    print("Numebr is even ")
-}
-else
-{
-    print("Nmebr is odd")
-}
+let x = readLine()
+
+
+print("Your Number of wives is " + x!)
+
+//let x = 11
+//
+//if (x % 2 == 0 ) {
+//
+//    print("Numebr is even ")
+//}
+//else
+//{
+//    print("Nmebr is odd")
+//}
